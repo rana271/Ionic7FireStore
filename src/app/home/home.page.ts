ea4dca4be680bad0a6c8ef411b98ff616e02ad5f
Rana <rana@gmail.com>
2024-04-22 10:49:07 +0530

    Firestore commit with Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Firestore, collection, collectionData, doc, docData, 
+  addDoc, deleteDoc, updateDoc } from '@angular/fire/firestore';
+  import { Observable } from 'rxjs';
+  export interface Note {
+    id?: string;
+    name: string;
+    description: string;
+  }
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  notes: Note[] = [];
+  constructor(private firestore: Firestore) {
+    this.getNotes().subscribe(res => {
+      this.notes = res;
+    });
+  }
+  getNotes(): Observable<any[]> {
+    const notesRef = collection(this.firestore, 'note');
+    return collectionData(notesRef, { idField: 'id'}) as Observable<any[]>;
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDwrSIgIfr4d1aeqU3c4FNi1SmYvjb0Kxw',
+    authDomain: 'hello-8a90b.firebaseapp.com',
+    projectId: 'hello-8a90b',
+    storageBucket: 'hello-8a90b.appspot.com',
+    messagingSenderId: '152666479611',
+    appId: '1:152666479611:web:74ff718463fc8b6923895e',
+    measurementId: 'G-4DZ17D9083'
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
